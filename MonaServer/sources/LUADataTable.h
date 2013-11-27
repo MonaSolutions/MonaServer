8b3e5c56f6aa11d103606f54afd2ca4fdfe5d103
Mathieu <mathieu.poux@gmail.com>
2013-11-27 16:58:17 +0100

    LUADataTable.h forgotten on the previous commit

---
+++ MonaServer/sources/LUADataTable.h
@@ -0,0 +1,51 @@
+/*
+Copyright 2014 Mona
+mathieu.poux[a]gmail.com
+jammetthomas[a]gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License received along this program for more
+details (or else see http://www.gnu.org/licenses/).
+
+This file is a part of Mona.
+*/
+
+#pragma once
+
+#include "Script.h"
+#include "Mona/Database.h"
+#include <set>
+
+class LUADataTable {
+public:
+
+	static int Get(lua_State *pState);
+	static int Set(lua_State *pState);
+
+	static void Init(lua_State *pState, LUADataTable& table);
+	static int	Destroy(lua_State* pState);
+
+	LUADataTable(Mona::Database& database, const std::string& path) : newInsertion(false),count(0), path(path), database(database) {}
+
+	Mona::Database&			database;
+	const std::string		path;
+	Mona::UInt32			count;
+	std::set<std::string>	complexData;
+	bool					newInsertion;
+
+private:
+	static int NewIndex(lua_State *pState);
+
+	const Mona::UInt8* serialize(lua_State* pState, int index, Mona::UInt32& size);
+
+	std::string		_buffer;
+	
+};
+
