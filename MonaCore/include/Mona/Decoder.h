8e4dac60849d23df678d521cdbeb06cef803a3bf
mathieupoux <mathieu.poux@gmail.com>
2014-11-20 17:38:21 +0100

    - Add Decoder.h and WSDecoder.h files missing!

---
+++ MonaCore/include/Mona/Decoder.h
@@ -0,0 +1,233 @@
+/*
+Copyright 2014 Mona
+mathieu.poux[a]gmail.com
+jammetthomas[a]gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License received along this program for more
+details (or else see http://www.gnu.org/licenses/).
+
+This file is a part of Mona.
+*/
+
+#pragma once
+
+#include "Mona/Mona.h"
+#include "Mona/Invoker.h"
+#include "Mona/Logs.h"
+
+namespace Mona {
+
+namespace Events {
+	template<typename DecodedType>
+	struct OnDecoded : Event<void(DecodedType& decoded, const SocketAddress& address)> {};
+	struct OnDecodedEnd : Event<void()> {};
+};
+
+template<typename DecodedType>
+class Decoder : public virtual Object,
+	public Events::OnDecoded<DecodedType>,
+	public Events::OnDecodedEnd {
+
+public:
+	Decoder(Invoker& invoker, const char* name) : poolBuffers(invoker.poolBuffers), _name(name), _pThread(NULL), _poolThreads(invoker.poolThreads), _pDecoding(new Decoding<DecodedType>(invoker, name)),
+		onDecoding([this](Exception& ex, UInt8* data, UInt32 size) { return decoding(ex, data, size); }) {
+
+		_pDecoding->OnDecodedEnd::subscribe(*this);
+		_pDecoding->Events::OnDecoded<DecodedType>::subscribe(*this);
+		_pDecoding->OnDecoding::subscribe(onDecoding);
+	}
+
+	virtual ~Decoder() {
+		_pDecoding->OnDecoding::unsubscribe(onDecoding);
+		_pDecoding->Events::OnDecoded<DecodedType>::unsubscribe(*this);
+		_pDecoding->OnDecodedEnd::unsubscribe(*this);
+	}
+
+	UInt32 decode(PoolBuffer& pBuffer) {	
+		return decode(SocketAddress::Wildcard(), pBuffer);
+	}
+
+	UInt32 decode(const SocketAddress& address,PoolBuffer& pBuffer) {	
+		Exception ex;
+		std::lock_guard<std::mutex> lock(_pDecoding->_inMutex);
+		_pThread = _poolThreads.enqueue(ex, _pDecoding, _pThread);
+		if (ex)
+			ERROR(_name, ", ", ex.error());
+		_pDecoding->_input.emplace_back(poolBuffers);
+		_pDecoding->_addresses.emplace_back(address);
+		return _pDecoding->_input.back().swap(pBuffer).size();
+	}
+
+protected:
+
+	const PoolBuffers& poolBuffers;
+
+	template <typename ...Args>
+	void receive(Args&&... args) { _pDecoding->receive(args ...); }
+
+private:
+
+	// return the rest
+	virtual UInt32 decoding(Exception& ex, UInt8* data,UInt32 size) = 0;
+
+	struct OnDecoding : Event<UInt32(Exception& ex, UInt8* data, UInt32 size)> {};
+
+	template<typename>
+	class Decoding : public WorkThread, private Task, public virtual Object,
+		public OnDecoding,
+		public Events::OnDecoded<DecodedType>,
+		public Events::OnDecodedEnd {
+	public:
+
+		class OutType : public virtual Object {
+		public:
+			template <typename ...Args>
+			OutType(const PoolBuffers& poolBuffers,Args&&... args) : pBuffer(poolBuffers),decoded(args ...) {}
+
+			DecodedType	decoded;
+			PoolBuffer	pBuffer;
+		};
+
+		Decoding(Invoker& invoker, const char* name) : _new(false), _pBuffer(invoker.poolBuffers), WorkThread(name), Task(invoker) {}
+
+		virtual ~Decoding() {
+			for (OutType* pOut : _output)
+				delete pOut;
+		}
+
+		template <typename ...Args>
+		void receive(Args&&... args) {
+			_new = true;
+			_pNewOut = new OutType(_pBuffer.poolBuffers,args ...);
+			std::lock_guard<std::mutex> lock(_outMutex);
+			_output.emplace_back(_pNewOut);
+		}
+
+		bool run(Exception& exc) {
+
+			_new = false;
+			SocketAddress address;
+		
+			while (true) {
+				PoolBuffer pBuffer(_pBuffer.poolBuffers);
+				{
+					std::lock_guard<std::mutex> lock(_inMutex);
+					if (_input.empty())
+						break;
+					pBuffer.swap(_input.front());
+					_input.pop_front();
+					address = _addresses.front();
+					_addresses.pop_front();
+				}
+
+				// add general buffer in front
+				if (!_pBuffer.empty()) {
+					if (!pBuffer.empty())
+						_pBuffer->append(pBuffer.data(),pBuffer.size());
+					pBuffer.swap(_pBuffer);
+					_pBuffer.release();
+				}
+
+				UInt32 consumed(0);
+			
+				while(consumed<pBuffer.size()) { // while everything is not consumed
+
+					if (consumed) {
+						if (_pNewOut) {
+							_pBuffer->resize(pBuffer.size()-consumed);
+							memcpy(_pBuffer->data(), pBuffer->data()+consumed, _pBuffer->size());
+							pBuffer->resize(consumed);
+							_pNewOut->pBuffer.swap(pBuffer);
+							pBuffer.swap(_pBuffer);
+						} else
+							pBuffer->clip(consumed);
+					}
+
+					Exception ex;
+					_pNewOut = NULL;
+					consumed = OnDecoding::raise<0xFFFFFFFF>(ex, pBuffer->data(),pBuffer->size());
+
+					if (ex)
+						ERROR(name, ", ", ex.error())
+					else if (address) {
+						std::lock_guard<std::mutex> lock(_outMutex);
+						_newAddress = address;
+					}
+
+					if (!consumed) {
+						_pBuffer.swap(pBuffer); // memorize the rest in general buffer
+						break;
+					}
+
+					if (_pNewOut && consumed >= pBuffer.size()) {
+						_pNewOut->pBuffer.swap(pBuffer);
+						break;
+					}
+
+				}
+			}
+
+			if (_new)
+				waitHandle();
+
+			return true;
+		}
+
+
+		void handle(Exception& ex) {
+			OutType* pOut(NULL);
+			SocketAddress address;
+
+			while (true) {
+				{
+					std::lock_guard<std::mutex> lock(_outMutex);
+					if (_output.empty())
+						break;
+					pOut = _output.front();
+					_output.pop_front();
+					if (_newAddress) {
+						address = _newAddress;
+						_newAddress.reset();
+					}
+				}
+				Events::OnDecoded<DecodedType>::raise(pOut->decoded,address);
+				delete pOut;
+			}
+			if (pOut) // at less one has been done
+				Events::OnDecodedEnd::raise();
+		}
+
+		bool									_new;
+		OutType*								_pNewOut;
+		PoolBuffer								_pBuffer;
+
+		std::mutex								_inMutex;
+		std::deque<PoolBuffer>					_input;
+		std::deque<SocketAddress>				_addresses;
+		
+		std::mutex								_outMutex;
+		std::deque<OutType*>					_output;
+		SocketAddress							_newAddress;
+	};
+
+	typename Decoding<DecodedType>::OnDecoding::Type		onDecoding;
+	typename Decoding<DecodedType>::OnDecoded::Type			onDecoded;
+	typename Decoding<DecodedType>::OnDecodedEnd::Type		onDecodeEnd;
+
+	const std::shared_ptr<Decoding<DecodedType>>	_pDecoding;
+
+	PoolThread*										_pThread;
+	PoolThreads&									_poolThreads;
+	const char*										_name;
+};
+
+
+} // namespace Mona
