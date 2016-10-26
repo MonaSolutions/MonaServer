66ee355654d35d73fb56977903a228a77d08f477
thomasjammet <jammetthomas@gmail.com>
2016-10-26 09:42:24 +0200

    - correction of last commit, we where linking with wrong version of bootstrap

--- doc/conf.py
+++ doc/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MonaServer'
-copyright = u'2015, Mathieu Poux & Thomas Jammet'
+copyright = u'2016, Mathieu Poux & Thomas Jammet'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
